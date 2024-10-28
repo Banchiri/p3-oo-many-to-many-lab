edfc0d4f87394ead7dacc47087e213287ea9db27
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-10-28 14:38:28 +0300

    works on a many-to-many relationship between an author, a book and a contract

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,13 +1,35 @@
 class Author:
+    all = [] #class variable to track all Author instances
     def __init__(self, name):
         self.name = name
+        Author.all.append(self)
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    def sign_contract(self, book, date, royalties):
+        #create a new Contract object and return it
+        new_contract = Contract(self, book, date, royalties)
+        return new_contract
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
     def __str__(self):
         return f"Author: {self.name}"
-
+    
+   
 
 class Book:
+    all = []
     def __init__(self, title):
        self.title = title 
+       Book.all.append(self)
+    def contracts(self):
+        # Return a list of contracts where the book is self
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        # Return a list of authors associated with the book via contracts
+        return [contract.author for contract in self.contracts()]
     def __str__(self):
         return f"Book: {self.title}"
 
@@ -26,7 +48,7 @@ class Contract:
     @author.setter
     def author(self,value): 
         if isinstance(value, Author):
-            self._author == value
+            self._author = value
         else:
 
             raise Exception("the author must be an instance of the Author class") 
@@ -58,3 +80,6 @@ class Contract:
             self._royalties = value
         else:
             raise Exception("The royalties property should be a number that represents the percentage of royalties that the author will receive for the book.")
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
